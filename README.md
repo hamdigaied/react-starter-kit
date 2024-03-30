e5db5bcf0ed8abc0b03d8e569ea6fc786c577539
Hamdi Gaied <hamdi.gaied.si@gmail.com>
2024-03-31 00:29:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Starter Kit
+
+This template provides a minimal setup to started with a ready-to-use template baked using :
+- Nodejs 20
+- React 18
+- TypeScript 5
+- Vite 5
---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import reactLogo from '/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://github.com/hamdigaied/react-starter-kit" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>React Starter Kit</h1>
+    </>
+  )
+}
+
+export default App
